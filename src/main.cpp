3b5fe5e8c83389be5646d089ace506f89c83db4f
Jody <jodystocks01@gmail.com>
2022-12-28 21:08:23 +0200

    Added User Input

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,36 @@ namespace gtn
 	}
 
 
-	int random_in_range(int lo, int hi) 
+	int random_in_range(int lo, int hi)
 	{
 		//std::uniform_real_distribution  // For floats and doubles.
 		//static auto engine = std::default_random_engine{};
 		static auto engine = std::default_random_engine{ std::random_device{}() };
-		auto distribution = std::uniform_int_distribution<int>{ lo, hi };		
+		auto distribution = std::uniform_int_distribution<int>{ lo, hi };
 		return distribution(engine);
+	}
+
+	[[nodiscard]] int user_input(std::string const& msg)
+	{
+		while (true)
+		{
+			print_log(msg);
+
+			std::string input{};
+			int ret{};
+
+			std::getline(std::cin, input); // Delimit - Input limit.
+
+			try
+			{
+				ret = std::stoi(input);
+				return ret;
+			}
+			catch (const std::exception&)
+			{
+				print_log("The value input is not an interger, Please try again with whole numbers");				
+			}
+		}
 	}
 }
 
@@ -38,4 +61,11 @@ int main()
 		int const rand_val = random_in_range(0, 10);
 		print_log(std::to_string(rand_val));
 	}
+
+	int user_num = user_input("Please enter a number");
+
+	std::string msg = "User Input: ";
+	msg += std::to_string(user_num);
+
+	print_log(msg);
 }
